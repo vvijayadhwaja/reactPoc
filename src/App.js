e21a8463c71634fee03d5d54ba29a4000a3e202d
vijayadhwaja <vijayadhwaja@digital-result.com>
2023-06-29 16:26:43 +0200

    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import PingComponent from "./components/PingComponent";
 function App() {
   return (
     <div>
-      <h1>Today's Todos!!</h1>
+      <h1>Vaarini's Today's Todos!!</h1>
       <Todo text='Learn react'/>
       <Todo text='Setup postgres'/>
       <Todo text='Update progress to Ludwig'/>
