85ebaee44443e9cc3534ff03f1ff57361b5510f2
vijayadhwaja <vijayadhwaja@digital-result.com>
2023-07-04 12:47:53 +0200

    demo

--- test.js/index.test.js
+++ test.js/index.test.js
@@ -1,1 +1,11 @@
 // write the test code here ...
+import Todos from "../src/components/Todo";
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import Fetch from './fetch'
+
+
+
+
+console.log(Todos)
