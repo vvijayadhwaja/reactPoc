a0f5f705186365b728187c42604a59f760f67bb5
vijayadhwaja <vijayadhwaja@digital-result.com>
2023-07-24 13:58:16 +0200

    test pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,39 @@
+locals {
+  stack_name = "${var.project_name}-${var.region}-${var.environment}"
+  default_tags = {
+    Terraform   = true
+    Project     = var.project_name
+    Region      = var.region
+    Environment = var.environment
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.44.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.3"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+  default_tags {
+    tags = local.default_tags
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "devopscodesacademyremotestate"
+    key            = "infrastructure/eu-central-1/terraform.tfstate"
+    region         = "eu-central-1"
+  }
+}
